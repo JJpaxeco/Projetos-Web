2c4893880c7046cf5458b5d927fbd097b8baa03f
JJpaxeco <jjpaxeco@hotmail.com>
2023-05-16 20:36:35 -0400

    js

--- javascript/exemplo2.js
+++ javascript/exemplo2.js
@@ -1,4 +1,7 @@
 var  n1 = parseInt(prompt('Informe o primeiro valor: '));
 var  n2 = parseInt(prompt('Informe o segundo valor: '));
-alert(n1+n2);
-console.log(n1+n2)
+//alert(n1+n2);
+result.innerHTML = n1+n2;
+var resp = confirm("Podemos continuar?");
+document.write("<p>"+resp+"</p>");
+//console.log(n1+n2)
