ce5f3bb7d2c0d5a5436e5b8fd9b75d9ebc338c11
JJpaxeco <jjpaxeco@hotmail.com>
2023-05-16 20:37:16 -0400

    js2

--- javascript/exemplo2.js
+++ javascript/exemplo2.js
@@ -2,6 +2,7 @@ var  n1 = parseInt(prompt('Informe o primeiro valor: '));
 var  n2 = parseInt(prompt('Informe o segundo valor: '));
 //alert(n1+n2);
 result.innerHTML = n1+n2;
+//result.innerHTML = n1+n2; serve para inserir o resultado em um elemento do HTML
 var resp = confirm("Podemos continuar?");
 document.write("<p>"+resp+"</p>");
 //console.log(n1+n2)
