bfa752f14e3b74908f1afe13bc0fca1c11ada073
JJpaxeco <jjpaxeco@hotmail.com>
2023-06-06 20:37:59 -0400

    .

--- javascript/formulario2.js
+++ javascript/formulario2.js
@@ -11,3 +11,19 @@ aperitivo = document.getElementById('aperit')
 aperitivo.onchange = function(){
     this.checked ? alert("Ok, vou providenciar!") : alert('Até a próxima');
 };
+
+let sexo = document.getElementsByName('sexo');
+let hobbies = document.getElementsByName('hobbies');
+
+btSexo.onclick = function(){
+    let mens = "";
+    mens += 'Sexo:';
+    sexo.forEach(s =>{
+        s.checked ? mens += s.value + '\n': undefined;
+    });
+    mens += '\n Hobbies: \n';
+    hobbies.forEach(h =>{
+        mens += h.checked ? h.value + '\n': '';
+    });
+    mensagem.text = mens;
+};
