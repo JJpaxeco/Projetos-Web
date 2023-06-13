53e35bee9dc93af18983a75d6732dd9ee057147f
JJpaxeco <jjpaxeco@hotmail.com>
2023-06-13 19:58:58 -0400

    ..

--- javascript/exercicios/exercicio3/exercicio3.js
+++ javascript/exercicios/exercicio3/exercicio3.js
@@ -1,11 +1,9 @@
 function converterTexto() {
     var input = document.getElementById('texto');
     var span = document.getElementById('contador');
-
     var texto = input.value;
     var textoMaiusculo = texto.toUpperCase();
     var contador = texto.length;
-
     input.value = textoMaiusculo;
     span.textContent = "Número de caracteres: " + contador;
 }
